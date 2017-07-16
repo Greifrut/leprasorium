09fad763b0d121750a02dc135be38fedfe6f6018
Greifrut <artur.bunko@gmail.com>
2017-07-16 16:20:54 +0300

    fix

--- app.rb
+++ app.rb
@@ -60,5 +60,8 @@ end
 get '/details/:post_id' do
 	post_id = params[:post_id]
 
-	erb "Displaying information for post with id #{post_id}"
+	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	@row = results[1]
+
+	erb :details
 end
